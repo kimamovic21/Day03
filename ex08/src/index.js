c83805093fce32dbc34e348b256dd0d5af93ef04
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-10 23:04:11 +0100

    Update index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -4,6 +4,8 @@ function main(numOne, numTwo) {
   return sum; 
 }
 
-console.log(main(numOne, numTwo));
+console.log(main(15, 8));
+console.log(main(5, 28));
+console.log(main(150, 425));
 
 module.exports = main;
