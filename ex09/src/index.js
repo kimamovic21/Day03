413c8c4a8d061c4841446c0dbee6eb0fbf90439d
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-10 23:05:23 +0100

    Update index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -4,6 +4,8 @@ function main(numOne, numTwo) {
   return difference; 
 }
 
-console.log(main(numOne, numTwo));
+console.log(main(15, 8 ));
+console.log(main(25, 8 ));
+console.log(main(425, 120));
 
 module.exports = main;
