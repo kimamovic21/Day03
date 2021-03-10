9ee7196b0a9f2d6f7257cb36382c726a4e285737
kimamovic21 <79793398+kimamovic21@users.noreply.github.com>
2021-03-10 23:06:16 +0100

    Update index.js

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -4,6 +4,8 @@ function main(numOne, numTwo) {
   return product; 
 }
 
-console.log(main(numOne, numTwo));
+console.log(main(15, 3));
+console.log(main(25, 8 ));
+console.log(main(425, 10));
 
 module.exports = main;
